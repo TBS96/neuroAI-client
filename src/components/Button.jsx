2e92fd94cd4d2e50d7520f73de1d83e65f2a364b
TBS96 <9tbs6@proton.me>
2025-01-16 01:42:28 +0530

    create component Button and wrap it in index.js
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Button({
+    children,
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'text-white',
+    className = '',
+    ...props
+}) {
+    return (
+        <button className={`skeleton btn px-4 py-2 rounded-lg transition-all duration-200 ${bgColor} ${textColor} ${className}`} {...props}>
+            {children}
+        </button>
+    )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Input from "./Input";
 import Header from "./Header/Header";
 import Container from "./container/Container";
+import Button from "./Button";
 
 export {
     Input,
     Header,
     Container,
+    Button,
 }
