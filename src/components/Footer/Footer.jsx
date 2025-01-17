91767e4a82edb59c346b4e59242972cb9bbbd453
TBS96 <9tbs6@proton.me>
2025-01-18 04:31:31 +0530

    revamped component Footer.jsx
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../Logo';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Logo } from '../index'
 
 function Footer() {
-
     const footerLinks = [
         {
-            title: 'Social',
+            title: "Social",
             links: [
                 {
-                    name: 'GitHub',
-                    path: 'https://github.com/tbs96',
-                    icon: (<svg className='text-gray-800' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' viewBox='0 0 24 24'>
-                        <path fill-rule='evenodd' d='M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z' clip-rule='evenodd' />
-                    </svg>)
+                    name: "GitHub",
+                    path: "https://github.com/tbs96",
+                    icon: (
+                        <svg
+                            className="text-gray-800"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    ),
                 },
                 {
                     name: 'LinkedIn',
@@ -48,25 +61,89 @@ function Footer() {
         },
     ];
 
-    return (
-        <footer className='bg-gray-800 text-white py-10 border-t-2 border-t-gray-700'>
-            <div className='container mx-auto px-4'>
-                <div className='flex flex-wrap justify-between items-center'>
+    const additionalLinks = [
+        {
+            title: "Quick Links",
+            links: ["Features", "How It Works", "Testimonials"],
+        },
+        {
+            title: "Company",
+            links: ["About Us", "Remedy", "Contact"],
+        },
+        {
+            title: "Legal",
+            links: ["Privacy Policy", "Terms of Service", "Cookie Policy"],
+        },
+    ];
 
-                    <div className='w-full md:w-1/3 text-center border-b md:border-none pb-8 md:pb-0'>
-                        <Link to='/'>
-                            <Logo width='100px' />
+    const teamMembers = [
+        {
+            name: "Mahvish Ruhi",
+            url: "https://github.com/Mahvish16",
+            className: "btn btn-ghost hover:underline underline-offset-4 decoration-error hover:text-pink-400",
+        },
+        {
+            name: "Sayan Bhattacharya",
+            url: "https://github.com/Sayan-ezioo",
+            className: "btn btn-ghost hover:underline underline-offset-4 decoration-amber-500 hover:text-error",
+        },
+        {
+            name: "Prantik Ghosh",
+            url: "https://github.com/tbs96",
+            className: "btn btn-ghost hover:underline underline-offset-4 decoration-green-500 hover:text-yellow-400",
+        },
+        {
+            name: "Sanchita Kar",
+            url: "https://github.com/",
+            className: "btn btn-ghost hover:underline underline-offset-4 decoration-pink-500 hover:text-violet-400",
+        },
+        {
+            name: "Arghyakamal Ghosh",
+            url: "https://github.com/",
+            className: "btn btn-ghost hover:underline underline-offset-4 decoration-violet-500 hover:text-lime-500",
+        },
+    ];
+
+    return (
+        <footer className="bg-gray-800 text-white py-10 border-t-2 border-t-gray-700">
+            <div className="container mx-auto px-4">
+                {/* Upper Footer */}
+                <div className="flex flex-wrap justify-between items-center">
+                    {/* Logo */}
+                    <div className="w-full md:w-1/3 text-center border-b md:border-none pb-8 md:pb-0">
+                        <Link to="/">
+                            <Logo width="100px" />
                         </Link>
                     </div>
 
+                    {/* Footer Sections */}
+                    {additionalLinks.map((section, index) => (
+                        <div key={index} className="w-full md:w-auto text-center">
+                            <h3 className="font-semibold text-lg text-gray-400 uppercase mb-4">{section.title}</h3>
+                            <ul className="mt-2">
+                                {section.links.map((link, idx) => (
+                                    <li key={idx} className="mt-1 hover:underline underline-offset-4 hover:translate-x-2 transition-all duration-300">
+                                        <Link to='/'>
+                                            {link}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="mt-8 text-center border-t border-t-gray-700 pt-4 text-sm"></div>
+                {/* Social Links */}
+                <div className="flex flex-wrap justify-center mt-8">
                     {footerLinks.map((section) => (
-                        <div key={section.title} className='w-full md:w-auto text-center'>
-                            <h3 className='text-gray-400 uppercase font-semibold my-4 md:mb-4'>
+                        <div key={section.title} className="text-center">
+                            <h3 className="text-gray-400 uppercase font-semibold mb-4">
                                 {section.title}
                             </h3>
-                            <ul className='md:flex flex-wrap justify-center'>
+                            <ul className="flex justify-center">
                                 {section.links.map((link) => (
-                                    <li key={link.name} className='p-4'>
+                                    <li key={link.name} className="p-4">
                                         <Link
                                             to={link.path}
                                             className='text-gray-400 btn hover:glass hover:scale-110 rounded-2xl'
@@ -81,46 +158,27 @@ function Footer() {
                     ))}
                 </div>
 
-                <div className='mt-8 text-center border-t border-t-gray-700 pt-4 text-sm'>
+                {/* Lower Footer */}
+                <div className="mt-8 text-center border-t border-t-gray-700 pt-4 text-sm">
                     <p>
-                        &copy; 2025 <Link to='/' className='text-blue-400 hover:text-blue-500'>neuroAI</Link> | All Rights Reserved by{' '}
-                        <div className='md:flex flex-wrap justify-center'>
-                            <Link
-                                to='https://github.com/Mahvish16'
-                                className='btn btn-ghost hover:underline underline-offset-4 decoration-error hover:text-pink-400'
-                                target='_blank'
-                            >
-                                Mahvish Ruhi
-                            </Link>
-                            <Link
-                                to='https://github.com/Sayan-ezioo'
-                                className='btn btn-ghost hover:underline underline-offset-4 decoration-amber-500 hover:text-error'
-                                target='_blank'
-                            >
-                                Sayan Bhattacharya
-                            </Link>
-                            <Link
-                                to='https://github.com/tbs96'
-                                className='btn btn-ghost hover:underline underline-offset-4 decoration-green-500 hover:text-yellow-400'
-                                target='_blank'
-                            >
-                                Prantik Ghosh
-                            </Link>
-                            <Link
-                                to='https://github.com/'
-                                className='btn btn-ghost hover:underline underline-offset-4 decoration-pink-500 hover:text-violet-400'
-                                target='_blank'
-                            >
-                                Sanchita Kar
-                            </Link>
-                            <Link
-                                to='https://github.com/'
-                                className='btn btn-ghost hover:underline underline-offset-4 decoration-violet-500 hover:text-lime-500'
-                                target='_blank'
-                            >
-                                Arghyakamal Ghosh
-                            </Link>
-                        </div>
+                        &copy; 2025{" "}
+                        <Link to="/" className="text-blue-400 hover:text-blue-500">
+                            neuroAI
+                        </Link>{" "}
+                        | All Rights Reserved by{" "}
+                        {teamMembers.map((member, index) => (
+                            <div className="flex md:inline-flex justify-center" key={index}>
+                                <Link
+                                    to={member.url}
+                                    className={member.className}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    {member.name}
+                                </Link>
+                                {index < teamMembers.length - 1 && " "}
+                            </div>
+                        ))}
                     </p>
                 </div>
             </div>
