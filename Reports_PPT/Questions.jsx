86658f7674bfbc99671ddb470d82078f7582b50d
TBS96 <9tbs6@proton.me>
2025-03-10 22:36:28 +0530

    Q/A jsx files added
    not yet integrated
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ Reports_PPT/Questions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Questions as QuestionsPage } from '../components'
+
+function Questions() {
+    return (
+        <div>
+            <QuestionsPage></QuestionsPage>
+        </div>
+    )
+}
+
+export default Questions
---
+++ Reports_PPT/Results.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Questions as QuestionsPage } from '../components'
+
+function Results() {
+    return (
+        <div>
+            <QuestionsPage></QuestionsPage>
+        </div>
+    )
+}
+
+export default Results
