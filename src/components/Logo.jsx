f66b5c7b3bbd0b7c9f9bc5733419c267bea5c4d3
TBS96 <9tbs6@proton.me>
2025-03-30 19:21:58 +0530

    fix: update Logo component for improved styling and accessibility
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
     return (
-        <a href='/' className=''>
+        <a href='/' className='btn btn-ghost'>
             <img 
                 src='https://i.ibb.co/R4Yv6dLV/android-chrome-192x192-removebg-preview.png'
                 alt='NeuroAI Logo'
-                className='h-16'
+                className=''
                 style={{ width: width === '100%' ? 'auto' : 'width' }}
             />
         </a>
