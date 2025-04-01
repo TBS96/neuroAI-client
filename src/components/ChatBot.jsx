294051aaba166ebd94a6b130e440cd0f169206e0
TBS96 <9tbs6@proton.me>
2025-04-01 22:37:56 +0530

    refactor: update ChatBot component layout and integrate Input and Button components
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client'
+import { Button, Input } from './index';
 
 let socket;
 const CONNECTION_PORT = 'localhost:3001/';
@@ -46,49 +47,116 @@ const ChatBot = () => {
     };
 
     return (
-        <div className='grid place-items-center h-screen bg-[#2a2730]'>
-            {!loggedIn ? (
-                <div className='w-[600px] h-[350px] border-4 border-blue-600 rounded-lg flex justify-center items-center flex-col'>
+        <div className='grid place-items-center min-h-screen tab-content overflow-x-auto scroll-auto'>
 
-                    <div className='text-gray-200'>
-                        <input type="text" placeholder='Name...' onChange={(e) => { setUserName(e.target.value) }} className='m-10 w-52 h-10 border-2 border-[#0091ff] bg-transparent focus:outline-none pl-2 text-lg rounded-lg' />
-                        <input type="text" placeholder='Room...' onChange={(e) => { setRoom(e.target.value) }} className='m-10 w-52 h-10 border-2 border-[#0091ff] bg-transparent focus:outline-none pl-2 text-lg rounded-lg' />
-                    </div>
+            <div className='h-96 border mx-auto w-full max-w-6xl p-2 rounded-lg flex flex-col'>
 
-                    <button onClick={connectToRoom} className='w-52 h-12 bg-[#0091ff] text-white text-lg mt-11 hover:bg-[#037edc] rounded-lg'>Enter Chat</button>
-
-                </div>
-            ) : (
-                // chatContainer
-                <div className='w-[600px] h-[350px] border-4 border-blue-600 rounded-lg flex flex-col text-gray-300'>
-
-                    {/* messages */}
-                    <div className='flex-[80%] w-full overflow-y-auto p-4 space-y-2'>
-                        {messageList.map((val, key) => {
-                            const isCurrentUSer = val.author === userName;
-                            return (
-                                // messageContainer
-                                <div key={key} className={`flex ${isCurrentUSer ? 'justify-end' : 'justify-start'} my-2`} id={isCurrentUSer ? 'You' : val.author}>
-                                    {' '}
-                                    <div className={`max-w-xs p-3 rounded-lg ${isCurrentUSer ? 'bg-blue-500 text-white self-end' : 'bg-gray-300 text-black self-start'}`}>
-                                        <span className='block font-semibold'>{isCurrentUSer ? 'You' : val.author}</span>
-                                        <span>{val.message}</span>
-                                    </div>
+                {/* messages */}
+                <div className='flex-[80%] w-full overflow-y-auto p-4 space-y-2'>
+                    {messageList.map((val, key) => {
+                        const isCurrentUser = val.author === userName;
+                        return (
+                            // messageContainer
+                            <div key={key} className={`chat ${isCurrentUser ? 'chat-end' : 'chat-start'} my-2`} id={isCurrentUser ? 'You' : val.author}>
+                                {' '}
+                                <div className={`max-w-xs p-3 rounded-lg chat-bubble  ${isCurrentUser ? 'chat-bubble-primary' : 'chat-bubble-success'}`}>
+                                    <span className='block font-semibold'>{isCurrentUser ? 'You' : val.author}</span>
+                                    <span>{val.message}</span>
                                 </div>
-                            )
-                        })}
-                    </div>
-
-                    {/* messageInputs */}
-                    <div className='flex-[20%] flex flex-row'>
-                        <input type="text" onChange={(e) => { setMessage(e.target.value) }} value={message} className='flex-[80%] h-[calc(100%-5px)] border-t-4 border-[#0091ff] bg-transparent focus:outline-none text-lg rounded-l-lg pl-2' placeholder='Type a Message...' />
-                        <button onClick={sendMessage} className='flex-[20%] h-full bg-[#16c525] border-t-4 border-[#0091ff] text-white text-lg hover:bg-[#16c525cb] rounded-r-lg'>Send</button>
-                    </div>
-
+                            </div>
+                        )
+                    })}
                 </div>
-            )}
+
+                {/* messageInputs */}
+                <div className='join'>
+                    <Input 
+                        type='text'
+                        onChange={(e) => { setMessage(e.target.value) }}
+                        value={message}
+                        className='input join-item'
+                        placeholder='Type a Message...'
+                    />
+                    <Button 
+                        onClick={sendMessage}
+                        className='join-item rounded-r-full skeleton'
+                    >
+                        Send
+                    </Button>
+                </div>
+
+            </div>
+
         </div>
     )
 }
 
 export default ChatBot
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TODO: To be added later if required:
+// {!loggedIn ? (
+//     <div className='min-h-screen w-full py-16 border-4 border-blue-600 rounded-lg flex justify-center items-center flex-col'>
+
+//         <div className='flex justify-center items-center'>
+//             <div className='space-y-5'>
+//                 <Input
+//                     label='Name:'
+//                     placeholder='Name...'
+//                     type='text'
+//                     onChange={(e) => { setUserName(e.target.value) }}
+//                     required
+//                 />
+//                 <Input
+//                     label='Room:'
+//                     placeholder='Room...'
+//                     type='text'
+//                     onChange={(e) => { setRoom(e.target.value) }}
+//                     required
+//                 />
+//             </div>
+//         </div>
+
+//         <Button type='submit' onClick={connectToRoom} className='my-5'>Enter Chat</Button>
+
+//     </div>
+// ) : (
+//     // chatContainer
+//     <div className='w-[600px] h-[350px] border-4 border-blue-600 rounded-lg flex flex-col text-gray-300'>
+
+//         {/* messages */}
+//         <div className='flex-[80%] w-full overflow-y-auto p-4 space-y-2'>
+//             {messageList.map((val, key) => {
+//                 const isCurrentUSer = val.author === userName;
+//                 return (
+//                     // messageContainer
+//                     <div key={key} className={`flex ${isCurrentUSer ? 'justify-end' : 'justify-start'} my-2`} id={isCurrentUSer ? 'You' : val.author}>
+//                         {' '}
+//                         <div className={`max-w-xs p-3 rounded-lg ${isCurrentUSer ? 'bg-blue-500 text-white self-end' : 'bg-gray-300 text-black self-start'}`}>
+//                             <span className='block font-semibold'>{isCurrentUSer ? 'You' : val.author}</span>
+//                             <span>{val.message}</span>
+//                         </div>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+
+//         {/* messageInputs */}
+//         <div className='flex-[20%] flex flex-row'>
+//             <input type="text" onChange={(e) => { setMessage(e.target.value) }} value={message} className='flex-[80%] h-[calc(100%-5px)] border-t-4 border-[#0091ff] bg-transparent focus:outline-none text-lg rounded-l-lg pl-2' placeholder='Type a Message...' />
+//             <button onClick={sendMessage} className='flex-[20%] h-full bg-[#16c525] border-t-4 border-[#0091ff] text-white text-lg hover:bg-[#16c525cb] rounded-r-lg'>Send</button>
+//         </div>
+
+//     </div>
+// )}
