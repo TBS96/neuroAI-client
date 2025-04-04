fd49bd19e44bf4a2085c28997f7856f9a71338bd
TBS96 <9tbs6@proton.me>
2025-04-04 19:09:08 +0530

    feat: enhance Login component with password visibility toggle and FAQ section
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom'
 import { Button, Input, Logo } from './index'
-import { Eye, EyeOff } from 'lucide-react';
+import { Eye, EyeClosed } from 'lucide-react';
 
 function Login() {
 
@@ -19,7 +19,7 @@ function Login() {
     const login = () => { };
 
     return (
-        <div className='flex items-center justify-center w-full my-8 px-4 sm:px-0'>
+        <div className='flex items-center justify-center w-full my-8 px-4 sm:px-0 scroll-smooth' id='signupTarget'>
             <div className={`mx-auto w-full max-w-lg bg-gray-100/10 rounded-xl p-10 border border-black/10`}>
                 <div className='mb-2 flex justify-center'>
                     <span className='inline-block w-full max-w-[100px]'>
@@ -29,7 +29,7 @@ function Login() {
                 <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
                 <p className='mt-2 text-center text-base text-black'>
                     Don&apos;t have any account?&nbsp;
-                    <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>
+                    <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:link'>
                         Sign Up
                     </Link>
                 </p>
@@ -56,24 +56,49 @@ function Login() {
                         <div className='relative'>
                             <Input
                                 label='Password: '
-                                placeholder='✪✪✪✪✪✪✪✪✪✪✪'
+                                placeholder='••••••••'
                                 type={showPass ? 'text' : 'password'}
                                 {...register('password', {
                                     required: true,
+                                    maxLength: 16,
+                                    minLength: 8
                                 })}
                             />
-                            <button 
+                            <button
                                 type='button'
                                 onClick={() => setShowPass(!showPass)}
                                 className='absolute right-3 top-1/2 hover:cursor-pointer active:translate-y-[1px] duration-300 transition-all'
                             >
-                                {showPass ? <EyeOff size={25} className='text-secondary' /> : <Eye size={25} className='text-primary' />}
+                                {!showPass ? <EyeClosed size={25} className='text-secondary' /> : <Eye size={25} className='text-primary' />}
                             </button>
                         </div>
+
+                        <span className='relative flex items-center justify-end'>
+                            <a href='#' className='link link-accent'>Forgotten Password?</a>
+                        </span>
 
                         <Button type='submit' className='w-full'>Sign in</Button>
                     </div>
                 </form>
+
+                <div className="join join-vertical bg-base-100 mt-5">
+                    <div className="collapse collapse-arrow join-item border-base-300 border">
+                        <input type="checkbox" name="my-accordion-4" />
+                        <div className="collapse-title font-semibold">How do I create an account?</div>
+                        <div className="collapse-content text-sm">Click the <a href='#signupTarget' className='font-bold text-primary link'>Sign Up</a> button in the top right corner and follow the registration process.</div>
+                    </div>
+                    <div className="collapse collapse-arrow join-item border-base-300 border">
+                        <input type="checkbox" name="my-accordion-4" />
+                        <div className="collapse-title font-semibold">I forgot my password. What should I do?</div>
+                        <div className="collapse-content text-sm">Click on <span className='font-bold'>"Forgotten Password"</span> above and follow the instructions sent to your email.</div>
+                    </div>
+                    <div className="collapse collapse-arrow join-item border-base-300 border">
+                        <input type="checkbox" name="my-accordion-4" />
+                        <div className="collapse-title font-semibold">How do I update my profile information?</div>
+                        <div className="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
