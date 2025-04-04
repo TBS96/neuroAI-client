12af3e9efdfca99d7ed80a5a12f5fc88bd07326b
TBS96 <9tbs6@proton.me>
2025-04-04 20:31:55 +0530

    fix: update text color class for improved accessibility in Login component
    `Don't have any account?`
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
                     </span>
                 </div>
                 <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
-                <p className='mt-2 text-center text-base text-black'>
+                <p className='mt-2 text-center text-base text-base-content/45'>
                     Don&apos;t have any account?&nbsp;
                     <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:link'>
                         Sign Up
