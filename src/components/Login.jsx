35ddf678445bcc8138a13026781d96aa9987aeae
TBS96 <9tbs6@proton.me>
2025-04-04 17:09:17 +0530

    feat: add lucide-react for password visibility toggle in Login component
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom'
 import { Button, Input, Logo } from './index'
+import { Eye, EyeOff } from 'lucide-react';
 
-function Login () {
+function Login() {
 
     const navigate = useNavigate();
 
@@ -15,25 +16,25 @@ function Login () {
 
     const [showPass, setShowPass] = useState(false);
 
-    const login = () => {};
+    const login = () => { };
 
     return (
         <div className='flex items-center justify-center w-full my-8 px-4 sm:px-0'>
-            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <div className={`mx-auto w-full max-w-lg bg-gray-100/10 rounded-xl p-10 border border-black/10`}>
                 <div className='mb-2 flex justify-center'>
                     <span className='inline-block w-full max-w-[100px]'>
                         <Logo width='100%' />
                     </span>
                 </div>
                 <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
-                <p className='mt-2 text-center text-base text-black/60'>
+                <p className='mt-2 text-center text-base text-black'>
                     Don&apos;t have any account?&nbsp;
                     <Link to='/signup' className='font-medium text-primary transition-all duration-200 hover:underline'>
                         Sign Up
                     </Link>
                 </p>
-                {error && 
-                    <p className='text-red-600 mt-8 text-center bg-error-content animate-pulse'>{error}</p>
+                {error &&
+                    <p className='text-error mt-8 text-center bg-error-content animate-pulse'>{error}</p>
                 }
                 <form onSubmit={handleSubmit(login)} className='mt-8 form-control'>
                     <div className='space-y-5'>
@@ -48,10 +49,11 @@ function Login () {
                                 }
                             })}
                         />
-                        {error && 
+                        {error &&
                             <p className='text-red-600 mt-8 text-center animate-pulse bg-red-100'>{data.email} doesn't exist in our database. Please Sign Up!</p>
                         }
-                        <div className='flex items-end sm:flex-col gap-2'>
+
+                        <div className='relative'>
                             <Input
                                 label='Password: '
                                 placeholder='✪✪✪✪✪✪✪✪✪✪✪'
@@ -60,9 +62,15 @@ function Login () {
                                     required: true,
                                 })}
                             />
-                            <input type='checkbox' className='hidden' id='show' onChange={() => setShowPass(!showPass)} />
-                            <label htmlFor="show" className='btn btn-square btn-outline'>{showPass ? 'hide' : 'show'}</label>
+                            <button 
+                                type='button'
+                                onClick={() => setShowPass(!showPass)}
+                                className='absolute right-3 top-1/2 hover:cursor-pointer active:translate-y-[1px] duration-300 transition-all'
+                            >
+                                {showPass ? <EyeOff size={25} className='text-secondary' /> : <Eye size={25} className='text-primary' />}
+                            </button>
                         </div>
+
                         <Button type='submit' className='w-full hover:bg-green-600'>Sign in</Button>
                     </div>
                 </form>
