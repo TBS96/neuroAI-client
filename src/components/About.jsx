0b72b32b70d29d3cf3f606be119337758471f74e
TBS96 <9tbs6@proton.me>
2025-04-05 18:59:48 +0530

    refactor: enhance About component layout and styling for improved user experience
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,46 +2,53 @@ import React from 'react'
 
 const About = () => {
     return (
-        <>
-            <title>neuroAI | About</title>
-            <div className='bg-cover bg-center bg-no-repeat' style={{ backgroundImage: "url('https://i.ibb.co/h1ms14V/team.png')" }}>
-
-                <section className='flex items-center justify-center min-h-screen text-gray-500 text-center py-16'>
-                    <div className='px-4'>
-                        <h2 className='text-4xl sm:text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>About Neuro AI</h2>
-                        <p className='text-lg sm:text-xl'>
-                            Revolutionizing Artificial Intelligence through neuro-inspired algorithms.
-                        </p>
-                    </div>
-                </section>
-
-                <section className='bg-white bg-opacity-80 py-16'>
-                    <div className='max-w-6xl mx-auto px-6 md:px-12 lg:px-16'>
-                        <h2 className='text-3xl sm:text-4xl font-semibold text-gray-800 text-center mb-8'>Who We Are</h2>
-                        <p className='text-gray-700 text-base sm:text-lg text-center mb-12'>
-                            Neuro AI is a leading innovator in the field of artificial intelligence, harnessing the power of neural networks to solve complex problems. Our team of experts is dedicated to pushing the boundaries of AI technology and creating solutions that can have a lasting impact on industries and society.
-                        </p>
-
-                        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
-                            <div className='bg-gray-100 glass hover:skeleton hover:backdrop-blur-sm p-6 rounded-lg shadow-md hover:shadow-xl hover:scale-105 duration-200 cursor-pointer'>
-                                <h3 className='text-2xl font-semibold text-gray-800 mb-4'>Our Mission</h3>
-                                <p className='text-gray-700'>
-                                    Our mission is to bring AI closer to human-level intelligence, revolutionizing various sectors such as healthcare, finance, and robotics, by creating neuro-inspired algorithms that enhance decision-making processes.
-                                </p>
-                            </div>
-
-                            <div className='bg-gray-100 glass hover:skeleton hover:backdrop-blur-sm p-6 rounded-lg shadow-md hover:shadow-xl hover:scale-105 duration-200 cursor-pointer'>
-                                <h3 className='text-2xl font-semibold text-gray-800 mb-4'>Our Vision</h3>
-                                <p className='text-gray-700'>
-                                    We aim to be at the forefront of AI research, developing technologies that improve the quality of life and contribute to global innovation. Our vision is to create a future where AI complements human potential for greater creativity and productivity.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
+        <div className='w-full'>
+            <div
+                className='hero min-h-screen bg-cover bg-center flex items-center justify-center text-center hero-overlay'
+                style={{ backgroundImage: "url('https://i.ibb.co/h1ms14V/team.png')" }}
+            >
+                <div className='md:hero-content bg-base-300/90 p-8 rounded-md hover:glass duration-300 transition-all ease-in-out hover:shadow-2xl animate-pulse hover:animate-none sticky top-1/8 z-[99] mx-4'>
+                    <h2 className='text-4xl sm:text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500'>
+                        Neuro AI
+                    </h2>
+                    <p className='text-lg sm:text-xl text-accent'>
+                        Revolutionizing Artificial Intelligence through neuro-inspired algorithms.
+                    </p>
+                </div>
             </div>
-        </>
+
+            <section className='bg-base-100 py-16'>
+                <div className='max-w-6xl mx-auto px-6 md:px-12 lg:px-16'>
+                    <h2 className='text-3xl sm:text-4xl font-semibold text-center mb-8 text-primary'>
+                        Who We Are
+                    </h2>
+                    <p className='text-base sm:text-lg text-center mb-12 text-secondary'>
+                        Neuro AI is a leading innovator in the field of artificial intelligence, harnessing the power of neural networks to solve complex problems. Our team of experts is dedicated to pushing the boundaries of AI technology and creating solutions that can have a lasting impact on industries and society.
+                    </p>
+
+                    <div className='grid gap-8 md:grid-cols-2'>
+                        {[
+                            {
+                                title: 'Our Mission',
+                                desc: 'Our mission is to bring AI closer to human-level intelligence, revolutionizing various sectors such as healthcare, finance, and robotics, by creating neuro-inspired algorithms that enhance decision-making processes.',
+                            },
+                            {
+                                title: 'Our Vision',
+                                desc: 'We aim to be at the forefront of AI research, developing technologies that improve the quality of life and contribute to global innovation. Our vision is to create a future where AI complements human potential for greater creativity and productivity.'
+                            }
+                        ].map(({ title, desc }, index) => (
+                            <div
+                                key={index}
+                                className='p-6 skeleton shadow-md rounded-md text-left hover:shadow-2xl transition-all duration-300 hover:rotate-2 hover:cursor-pointer'
+                            >
+                                <h3 className='text-2xl text-accent font-semibold mb-4'>{title}</h3>
+                                <p className='italic text-base'>{desc}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
