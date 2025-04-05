0df5f53083d77e610b282993ce0a5e0b388566b5
TBS96 <9tbs6@proton.me>
2025-04-05 17:35:17 +0530

    fix: add title element to About page for better SEO
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import { About as AboutComponent } from '../components/index'
 const About = () => {
     return (
         <>
+            <title>neuroAI | About</title>
             <AboutComponent />
         </>
     )
