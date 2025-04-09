f186738773a010e35911138e5315c49b1d038e71
TBS96 <9tbs6@proton.me>
2025-04-09 19:31:25 +0530

    feat: add AOS animation to FeaturesSection component for improved visual effects
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Home/FeaturesSection.jsx
+++ src/components/Home/FeaturesSection.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const FeaturesSection = () => {
     return (
         <>
-            <section className='py-16 my-16 bg-gray-100/50 hover:shadow-2xl transition-all duration-300 ease-in-out' id='features' data-aos='zoom-in-up'>
+            <section className='py-16 my-16 bg-gray-100/50 hover:shadow-2xl transition-all duration-300 ease-in-out' id='features'>
                 <div className='container mx-auto px-6 md:px-12 text-center'>
-                    <h2 className='text-3xl md:text-4xl font-semibold'>
+                    <h2 className='text-3xl md:text-4xl font-semibold' data-aos='zoom-in-up'>
                         A better way to improve your mental health
                     </h2>
                     <div className='mt-12 grid gap-8 md:grid-cols-3'>
@@ -25,15 +25,17 @@ const FeaturesSection = () => {
                                 title: 'Instant Access',
                                 desc: '24/7 support at your fingertips, whenever and wherever you need it.',
                             },
-                        ].map((feature, index) => (
-                            <div key={index} className='bg-base-300 p-6 shadow-md hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 hover:cursor-pointer rounded-md text-left'>
-                                <img
-                                    src={feature.img}
-                                    alt={feature.title}
-                                    className='w-full h-48 object-cover rounded-md mb-4'
-                                />
-                                <h3 className='text-2xl font-medium'>{feature.title}</h3>
-                                <p className='mt-2'>{feature.desc}</p>
+                        ].map(({ img, title, desc }, index) => (
+                            <div data-aos='zoom-in-up'>
+                                <div key={index} className='bg-base-300 p-6 shadow-md hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 hover:cursor-pointer rounded-md text-left'>
+                                    <img
+                                        src={img}
+                                        alt={title}
+                                        className='w-full h-48 object-cover rounded-md mb-4'
+                                    />
+                                    <h3 className='text-2xl font-medium'>{title}</h3>
+                                    <p className='mt-2'>{desc}</p>
+                                </div>
                             </div>
                         ))}
                     </div>
