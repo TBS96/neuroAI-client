b9e774c53542e2efdd44046087a1d0a8cf2c1e90
TBS96 <9tbs6@proton.me>
2025-04-09 22:01:11 +0530

    fix: update AOS animation delays in About component for improved visual effects
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,10 @@ const About = () => {
                 style={{ backgroundImage: "url('https://i.ibb.co/h1ms14V/team.png')" }}
             >
                 <div className='md:hero-content bg-base-300/90 p-8 rounded-md hover:glass duration-300 transition-all ease-in-out hover:shadow-2xl animate-pulse hover:animate-none mx-4'>
-                    <h2 className='text-4xl sm:text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500' data-aos='fade-up' data-aos-delay='500'>
+                    <h2 className='text-4xl sm:text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500' data-aos='fade-up' data-aos-delay='700'>
                         Neuro AI
                     </h2>
-                    <p className='text-lg sm:text-xl text-accent' data-aos='fade-down' data-aos-delay='500'>
+                    <p className='text-lg sm:text-xl text-accent' data-aos='fade-down' data-aos-delay='700'>
                         Revolutionizing Artificial Intelligence through neuro-inspired algorithms.
                     </p>
                 </div>
@@ -22,7 +22,7 @@ const About = () => {
                     <h2 className='text-3xl sm:text-4xl font-semibold text-center mb-8 text-primary' data-aos='zoom-in-up'>
                         Who We Are
                     </h2>
-                    <p className='text-base sm:text-lg mb-12 text-secondary text-justify' data-aos='zoom-in-down'>
+                    <p className='text-base sm:text-lg mb-12 text-secondary text-justify' data-aos='zoom-in-down' data-aos-delay='700'>
                         Neuro AI is a leading innovator in the field of artificial intelligence, harnessing the power of neural networks to solve complex problems. Our team of experts is dedicated to pushing the boundaries of AI technology and creating solutions that can have a lasting impact on industries and society.
                     </p>
 
@@ -39,7 +39,7 @@ const About = () => {
                         ].map(({ title, desc }, index) => {
                             const aos = index === 0 ? 'fade-down' : 'fade-up';
                             return (
-                                <div key={index} data-aos={aos}>
+                                <div key={index} data-aos={aos}  data-aos-delay='700'>
                                     <div className='p-6 skeleton shadow-md rounded-md text-left hover:shadow-2xl transition-all duration-300 hover:-translate-y-1 hover:cursor-pointer'
                                     >
                                         <h3 className='text-2xl text-accent font-semibold mb-4'>{title}</h3>
