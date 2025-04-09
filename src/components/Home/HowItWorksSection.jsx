16875a5629c9ca1df689859250e486c38dbfaad4
TBS96 <9tbs6@proton.me>
2025-04-09 20:17:15 +0530

    feat: add AOS animations to HowItWorksSection component for enhanced visual effects
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Home/HowItWorksSection.jsx
+++ src/components/Home/HowItWorksSection.jsx
@@ -6,7 +6,7 @@ const HowItWorksSection = () => {
         <>
             <section className='py-16 my-16 bg-white/10 hover:shadow-2xl transition-all duration-300 ease-in-out'>
                 <div className='container mx-auto px-6 md:px-12 text-center'>
-                    <h2 className='text-3xl md:text-4xl font-semibold'>How It Works</h2>
+                    <h2 className='text-3xl md:text-4xl font-semibold' data-aos='fade-right'>How It Works</h2>
                     <div className='mt-12 grid gap-8 md:grid-cols-3'>
                         {[
                             {
@@ -27,17 +27,20 @@ const HowItWorksSection = () => {
                                 desc: 'Get to know about your concerns towards better mental health.',
                                 link: '/',
                             },
-                        ].map(({ number, title, desc, link }, index) => (
-                            <div key={index} className='flex flex-col items-center bg-base-300 p-6 rounded-md shadow-md hover:shadow-2xl transition-all duration-300 hover:translate-x-4 hover:cursor-pointer'>
-                                <Link to={link} className='block w-full'>
-                                    <div className='text-4xl font-bold bg-base-100 glass w-16 h-16 flex items-center justify-center rounded-full mb-4 mx-auto'>
-                                        {number}
-                                    </div>
-                                    <h3 className='text-2xl font-extrabold'>{title}</h3>
-                                    <p className='mt-2 text-gray-600 font-bold'>{desc}</p>
-                                </Link>
-                            </div>
-                        ))}
+                        ].map(({ number, title, desc, link }, index) => {
+                            const aos = index === 0 ? 'fade-up' : index === 1 ? 'zoom-in' : 'flip-up'
+                            return (
+                                <div key={index} data-aos={aos} data-aos-delay={index * 200} className='flex flex-col items-center bg-base-300 p-6 rounded-md shadow-md hover:shadow-2xl transition-all duration-300 hover:translate-x-4 hover:cursor-pointer'>
+                                    <Link to={link} className='block w-full'>
+                                        <div className='text-4xl font-bold bg-base-100 glass w-16 h-16 flex items-center justify-center rounded-full mb-4 mx-auto'>
+                                            {number}
+                                        </div>
+                                        <h3 className='text-2xl font-extrabold'>{title}</h3>
+                                        <p className='mt-2 text-gray-600 font-bold'>{desc}</p>
+                                    </Link>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
