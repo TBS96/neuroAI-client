db626eacb8b1995145a1448d40ad334d748dc3a4
TBS96 <9tbs6@proton.me>
2025-04-09 21:22:10 +0530

    fix: improve description in Test Now! section for clarity
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Home/HowItWorksSection.jsx
+++ src/components/Home/HowItWorksSection.jsx
@@ -18,7 +18,7 @@ const HowItWorksSection = () => {
                             {
                                 number: '2',
                                 title: 'Test Now!',
-                                desc: 'Test to get a better mental health',
+                                desc: 'Test to get a better mental health with our chatbot',
                                 link: '/chatbot',
                             },
                             {
@@ -30,14 +30,16 @@ const HowItWorksSection = () => {
                         ].map(({ number, title, desc, link }, index) => {
                             const aos = index === 0 ? 'fade-up' : index === 1 ? 'zoom-in' : 'flip-up'
                             return (
-                                <div key={index} data-aos={aos} data-aos-delay={index * 200} className='flex flex-col items-center bg-base-300 p-6 rounded-md shadow-md hover:shadow-2xl transition-all duration-300 hover:translate-x-4 hover:cursor-pointer'>
-                                    <Link to={link} className='block w-full'>
-                                        <div className='text-4xl font-bold bg-base-100 glass w-16 h-16 flex items-center justify-center rounded-full mb-4 mx-auto'>
-                                            {number}
-                                        </div>
-                                        <h3 className='text-2xl font-extrabold'>{title}</h3>
-                                        <p className='mt-2 text-gray-600 font-bold'>{desc}</p>
-                                    </Link>
+                                <div key={index} data-aos={aos} data-aos-delay={index * 200}>
+                                    <div className='flex flex-col items-center bg-base-300 p-6 rounded-md shadow-md hover:shadow-2xl transition-all duration-300 hover:translate-x-4 hover:cursor-pointer'>
+                                        <Link to={link} className='block w-full'>
+                                            <div className='text-4xl font-bold bg-base-100 glass w-16 h-16 flex items-center justify-center rounded-full mb-4 mx-auto'>
+                                                {number}
+                                            </div>
+                                            <h3 className='text-2xl font-extrabold'>{title}</h3>
+                                            <p className='mt-2 text-gray-600 font-bold'>{desc}</p>
+                                        </Link>
+                                    </div>
                                 </div>
                             )
                         })}
