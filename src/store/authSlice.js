a82e33b6c64fc945c5c43893e7f8b0667c6712e9
TBS96 <9tbs6@proton.me>
2025-04-29 23:48:58 +0530

    feat: implement login user thunk with loading and error handling in authSlice
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { loginUserApi } from '../api/authApi'
 
 // Load user from localStorage if available
 const userFromStorage = JSON.parse(localStorage.getItem('authData'));
@@ -6,22 +7,50 @@ const userFromStorage = JSON.parse(localStorage.getItem('authData'));
 const initialState = {
     status: userFromStorage ? true : false,
     userData: userFromStorage || null,
+    loading: false,
+    error: null,
 };
+
+// Thunk to handle login
+export const loginUser = createAsyncThunk('auth/loginUser', 
+    async (credentials, thunkAPI) => {
+        try {
+            const data = await loginUserApi(credentials);   // use the function from authApi.js
+            return data;
+        }
+        catch (error) {
+            return thunkAPI.rejectWithValue(error.response?.data || {message: 'Login failed'});
+        }
+    }
+);
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        login: (state, action) => {
-            state.status = true;
-            state.userData = action.payload.userData;
-            localStorage.setItem('authData', JSON.stringify(action.payload.userData));
-        },
         logout: (state) => {
             state.status = false;
             state.userData = null;
             localStorage.removeItem('authData');
         }
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(loginUser.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.loading = false;
+            state.status = true;
+            state.userData = action.payload;
+            localStorage.setItem('authData', JSON.stringify(action.payload));
+        })
+        .addCase(loginUser.rejected, (state, action) => {
+            state.loading = false;
+            state.status = false;
+            state.error = action.payload;
+        })
     }
 });
 
