afd975b1a63ae8bc7de61fa2fb6e456208d70e02
TBS96 <9tbs6@proton.me>
2025-04-30 21:18:55 +0530

    feat: add registerUser thunk with error handling and update authSlice for registration
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { loginUserApi } from '../api/authApi'
+import { loginUserApi, registerUserApi } from '../api/authApi'
 
 // Load user from localStorage if available
 const userFromStorage = JSON.parse(localStorage.getItem('authData'));
@@ -19,9 +19,24 @@ export const loginUser = createAsyncThunk('auth/loginUser',
             return data;
         }
         catch (error) {
-            console.error("Axios error:", error);
+            console.error(`Axios error: ${error}`);
             // console.log("Backend full error response:", error.response?.data); // Debugging backend error
             return thunkAPI.rejectWithValue(error.response?.data?.message || 'Login failed.');
+        }
+    }
+);
+
+// Thunk to handle register
+export const registerUser = createAsyncThunk('auth/registerUser', 
+    async (formData, thunkAPI) => {
+        try {
+            const data = await registerUserApi(formData);
+            return data;
+        }
+        catch (error) {
+            console.error(`Registration error: ${error}`);
+            console.log(`Backend error response: ${error.response?.data}`);
+            return thunkAPI.rejectWithValue(error.response?.data ?? {message: 'Registration Failed'});
         }
     }
 );
@@ -38,6 +53,7 @@ const authSlice = createSlice({
     },
     extraReducers: (builder) => {
         builder
+        // login cases
         .addCase(loginUser.pending, (state) => {
             state.loading = true;
             state.error = null;
@@ -54,9 +70,30 @@ const authSlice = createSlice({
             state.status = false;
             state.error = action.payload;
         })
+        // register cases
+        .addCase(registerUser.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        })
+        .addCase(registerUser.fulfilled, (state, action) => {
+            console.log(`Redux registerUser payload: ${action.payload}`);
+
+            state.loading = false;
+            state.status = true;
+            state.userData = action.payload;
+
+            if (action.payload) {
+                localStorage.setItem('authData', JSON.stringify(action.payload));
+            }
+        })
+        .addCase(registerUser.rejected, (state, action) => {
+            state.loading = false;
+            state.status = false;
+            state.error = action.payload;
+        })
     }
 });
 
-export const { login, logout } = authSlice.actions;
+export const { logout } = authSlice.actions;
 
 export default authSlice.reducer;
