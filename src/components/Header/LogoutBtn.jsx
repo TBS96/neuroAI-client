63720c2c95d247d278088e800d5ddc15e574e56c
TBS96 <9tbs6@proton.me>
2025-05-01 01:43:38 +0530

    feat: add LogoutBtn component for user logout functionality
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logout } from '../../store/authSlice'
+
+function LogoutBtn() {
+
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        dispatch(logout());
+        document.activeElement.blur();
+    };
+
+    const closeDropdown = () => {
+        document.activeElement.blur();
+    };
+
+    return (
+        <>
+            <div className='dropdown dropdown-bottom'>
+                <div tabIndex={0} role='button' className='btn btn-dash btn-error mb-2 md:mb-0 '>Logout</div>
+                <ul tabIndex={0} className='dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow-sm'>
+                    <li>
+                        <button onClick={logoutHandler} className='btn btn-sm btn-error w-full mb-2'>Confirm Logout</button>
+                    </li>
+                    <li>
+                        <button onClick={closeDropdown} className='btn btn-outline btn-sm w-full'>Cancel</button>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default LogoutBtn
