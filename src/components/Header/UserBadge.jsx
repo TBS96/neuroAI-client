e7e2a0a4c87748183d9cd9d097bca2fc6b748369
TBS96 <9tbs6@proton.me>
2025-05-07 20:02:38 +0530

    fix: update greeting in UserBadge component to include 'Hello' prefix
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Header/UserBadge.jsx
+++ src/components/Header/UserBadge.jsx
@@ -11,15 +11,15 @@ const UserBadge = () => {
 
     return (
 
-        <div className="drawer">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
-                <label htmlFor="my-drawer" className="btn font-bold px-5 py-1 ms-10 md:ms-40 rounded-2xl bg-base-300 drawer-button">{userName}!</label>
+        <div className='drawer'>
+            <input id='my-drawer' type='checkbox' className='drawer-toggle' />
+            <div className='drawer-content'>
+                <label htmlFor='my-drawer' className='btn font-bold px-5 py-1 ms-10 md:ms-40 rounded-2xl bg-base-300 drawer-button'>Hello {userName}!</label>
             </div>
 
-            <div className="drawer-side">
-                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu glass text-base-content min-h-full w-80 p-4">
+            <div className='drawer-side'>
+                <label htmlFor='my-drawer' aria-label='close sidebar' className='drawer-overlay'></label>
+                <ul className='menu glass text-base-content min-h-full w-80 p-4'>
                     <li>
                         <Link to={'/'} className='btn btn-block mb-2'>View Profile</Link>
                     </li>
