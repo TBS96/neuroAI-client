eb100bb1aac06d02cd2d4c7582f6f775e6655fe7
TBS96 <9tbs6@proton.me>
2025-05-07 19:59:46 +0530

    feat: restructure UserBadge component to include drawer layout and LogoutBtn
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Header/UserBadge.jsx
+++ src/components/Header/UserBadge.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom';
+import LogoutBtn from './LogoutBtn';
 
 const UserBadge = () => {
 
@@ -9,9 +10,25 @@ const UserBadge = () => {
     // console.log(`Hello ${userData.email}`);
 
     return (
-        <Link to={'/'} className='font-bold px-5 py-1 rounded-2xl bg-base-300'>
-            Hello {userName}!
-        </Link>
+
+        <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                <label htmlFor="my-drawer" className="btn font-bold px-5 py-1 ms-10 md:ms-40 rounded-2xl bg-base-300 drawer-button">{userName}!</label>
+            </div>
+
+            <div className="drawer-side">
+                <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu glass text-base-content min-h-full w-80 p-4">
+                    <li>
+                        <Link to={'/'} className='btn btn-block mb-2'>View Profile</Link>
+                    </li>
+                    <li>
+                        <LogoutBtn />
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 
 }
