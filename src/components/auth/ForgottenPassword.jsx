c869a371d008268e494bdfaee9da115efb4e5cba
TBS96 <9tbs6@proton.me>
2025-05-09 22:40:00 +0530

    feat: implement password reset functionality with error handling and loading state
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/auth/ForgottenPassword.jsx
+++ src/components/auth/ForgottenPassword.jsx
@@ -1,27 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Button, Input } from '../index'
 import { Link } from 'react-router-dom';
 import { ArrowLeftCircle } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux';
+import { requestPasswordReset } from '../../store/slices/authSlice';
 
 const ForgottenPassword = () => {
 
-    // const { register, handleSubmit, formState: { errors } } = useForm();
     const { register, handleSubmit } = useForm();
 
-    const [error, setError] = useState('');
+    const dispatch = useDispatch();
+
+    const { loading, error } = useSelector(state => state.auth);
+
+    const [successMessage, setSuccessMessage] = useState('');
 
     const [serverError, setServerError] = useState('');
+    // const [error, setError] = useState('');
 
-    const onSubmit = (data) => {
-        console.log(`Forgot Password Data: ${data}`);
-        // Later: Use Axios to POST data.email to backend
+    const sendPasswordResetLink = async (data) => {
+        setServerError('');
+        setSuccessMessage('');
+        try {
+            const res = await dispatch(requestPasswordReset(data.email)).unwrap();
+            setSuccessMessage(`Reset link sent! Check your inbox.`);
+            console.log(`Forgot Password Data: ${res}`);
+        }
+        catch (err) {
+            console.error(`Forgot Password Error: ${err}`)
+            setServerError(err?.message || 'Something went wrong. Please try again!');
+            setSuccessMessage('');
+        }
     };
+
+    useEffect(() => {
+        if (error) {
+            setSuccessMessage('');
+        }
+    }, [error]);
 
     return (
         <div className='min-h-screen flex items-center justify-center w-full px-4 sm:px-0 '>
             <form
-                onSubmit={handleSubmit(onSubmit)}
+                onSubmit={handleSubmit(sendPasswordResetLink)}
                 className='bg-gray-100/10 p-8 rounded-2xl shadow-2xl w-full max-w-md'
             >
                 <h2 className='text-2xl font-bold mb-6 text-center' data-aos='fade-up' data-aos-duration='700'>
@@ -29,6 +51,18 @@ const ForgottenPassword = () => {
                 </h2>
 
                 <div className='space-y-5'>
+
+                    {serverError && (
+                        <p className='text-red-600 mt-8 text-center animate-pulse bg-red-100 p-2 rounded'>
+                            {serverError}
+                        </p>
+                    )}
+
+                    {error && (
+                        <p className='text-red-600 mt-8 text-center animate-pulse bg-red-100 p-2 rounded'>
+                            {error}
+                        </p>
+                    )}
                     <Input
                         type='email'
                         placeholder='example@domain.com'
@@ -39,24 +73,24 @@ const ForgottenPassword = () => {
                                 matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || 'Email address must be a valid address',
                             }
                         })}
-                        data-aos='fade-right'
+                        data-aos='fade-up'
                         data-aos-duration='1000'
                     />
 
-                    {error && (
-                        <p className='text-red-600 mt-8 text-center animate-pulse bg-red-100 p-2 rounded'>
-                            {data.email} doesn't exist in our database.
+                    {successMessage && (
+                        <p className='text-green-500 mt-8 text-center animate-pulse bg-green-100 p-2 rounded'>
+                            {successMessage}
                         </p>
                     )}
 
-                    {serverError && (
-                        <p className='text-red-600 mt-8 text-center animate-pulse bg-red-100 p-2 rounded'>
-                            {serverError}
-                        </p>
-                    )}
-
-                    <Button type='submit' className='w-full' data-aos='fade-up' data-aos-duration='1200'>
-                        Send Reset Link
+                    <Button
+                        type='submit'
+                        disabled={loading}
+                        className='w-full'
+                        data-aos='fade-up'
+                        data-aos-duration='1200'
+                    >
+                        {loading ? 'Sending...' : 'Send Reset Link'}
                     </Button>
 
                     <Link to='/login' className='btn btn-dash btn-sm group flex items-center gap-2' data-aos='fade-right' data-aos-duration='1200' data-aos-delay='2000'>
