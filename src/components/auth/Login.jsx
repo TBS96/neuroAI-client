d38736c61d006b63235ef5755e91a68e8330cd1b
TBS96 <9tbs6@proton.me>
2025-05-15 19:47:29 +0530

    feat: update FAQ content for clarity and consistency in Login component
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -194,17 +194,17 @@ function Login() {
                     <div className="collapse collapse-arrow join-item border-base-300 border">
                         <input type="checkbox" name="my-accordion-4" />
                         <div className="collapse-title font-semibold">How do I create an account?</div>
-                        <div className="collapse-content text-sm">Click the <a href='#signupTarget' className='font-bold text-primary link'>Sign Up</a> button in the top right corner and follow the registration process.</div>
+                        <div className="collapse-content text-sm">Click the <a href='#signupTarget' className='font-bold text-primary hover:link'>Sign Up</a> button in the top right corner and follow the registration process.</div>
                     </div>
                     <div className="collapse collapse-arrow join-item border-base-300 border">
                         <input type="checkbox" name="my-accordion-4" />
                         <div className="collapse-title font-semibold">I forgot my password. What should I do?</div>
-                        <div className="collapse-content text-sm">Click on <span className='font-bold'>"Forgotten Password"</span> above and follow the instructions sent to your email.</div>
+                        <div className="collapse-content text-sm">Click on <span className='font-bold'>"Reset Password"</span> above and follow the instructions sent to your email.</div>
                     </div>
                     <div className="collapse collapse-arrow join-item border-base-300 border">
                         <input type="checkbox" name="my-accordion-4" />
                         <div className="collapse-title font-semibold">How do I update my profile information?</div>
-                        <div className="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
+                        <div className="collapse-content text-sm">Go to "View Profile" and select "Edit Profile" to make changes.</div>
                     </div>
                 </div>
 
