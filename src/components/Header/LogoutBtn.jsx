5fd1479492a37283e72efb86d01717c0bae42c5a
TBS96 <9tbs6@proton.me>
2025-06-12 00:22:27 +0530

    fix: enhance logout functionality with error handling and navigation
    feat: added LogOut icon from lucide-react
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { logoutUser } from '../../store/slices/authSlice'
+import { useNavigate } from 'react-router-dom'
+import { LogOut } from 'lucide-react'
 
 function LogoutBtn() {
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const refreshToken = useSelector(state => state.auth.refreshToken);
 
-    const logoutHandler = () => {
-        dispatch(logoutUser(refreshToken));
-        document.activeElement.blur();
+    const logoutHandler = async () => {
+        try {
+            await dispatch(logoutUser(refreshToken)).unwrap();
+            console.log(`Logout successfull, navigating to /login`);
+            document.activeElement.blur();
+            navigate('/login');
+        }
+        catch (err) {
+            console.error(`Logout failed: ${err}`);
+            dispatch({ type: 'auth/logout' });
+            document.activeElement.blur();
+            navigate('/login');
+        }
     };
 
     const closeDropdown = () => {
@@ -20,7 +32,9 @@ function LogoutBtn() {
     return (
         <>
             <div className='dropdown dropdown-bottom'>
-                <div tabIndex={0} role='button' className='btn btn-dash btn-block btn-error mb-2 md:mb-0' title='Logout'>Logout</div>
+                <div tabIndex={0} role='button' className='btn btn-dash btn-block btn-error mb-2 md:mb-0' title='Logout'>
+                    Logout <LogOut size={20} className=' ms-2' />
+                </div>
                 <ul tabIndex={0} className='dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow-sm'>
                     <li title='Confirm Logout'>
                         <button onClick={logoutHandler} className='btn btn-sm btn-error w-full mb-2'>Confirm Logout</button>
