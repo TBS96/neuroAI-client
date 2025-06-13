ea338399142548d0323e0bc4ab7e94877518d015
TBS96 <9tbs6@proton.me>
2025-06-13 19:47:09 +0530

    fix: add icons to navigation items in Header component for improved UI
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { useNavigate, NavLink } from 'react-router-dom'
 import { Logo } from '../index'
 import { useSelector } from 'react-redux';
 import UserBadge from './UserBadge';
-import { BrainIcon, BrainCircuitIcon } from 'lucide-react';
+import { BrainIcon, BrainCircuitIcon, Notebook, House, BadgeInfo, Contact, Sun, MoonStar, ChevronDown } from 'lucide-react';
 
 const themes = ['light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'haloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset', 'caramellatte', 'abyss', 'silk'];
 
@@ -25,22 +25,26 @@ const Header = () => {
         {
             name: 'Home',
             slug: '/',
-            active: true
+            active: true,
+            icon: (<House size={20} className='transition-all duration-300 group-hover:-translate-x-2' />),
         },
         {
             name: 'Messages',
             slug: '/chatbot',
             active: authStatus,
+            icon: (<House size={20} className='transition-all duration-300 group-hover:-translate-x-2' />)
         },
         {
             name: 'About',
             slug: '/about',
-            active: true
+            active: true,
+            icon: (<BadgeInfo size={20} className='transition-all duration-300 group-hover:-translate-x-2' />)
         },
         {
             name: 'Contact',
             slug: '/contact',
-            active: true
+            active: true,
+            icon: (<Contact size={20} className='transition-all duration-300 group-hover:-translate-x-2' />)
         },
         // {
         //     name: 'Login',
@@ -52,6 +56,7 @@ const Header = () => {
             name: 'Register',
             slug: '/register',
             active: !authStatus,
+            icon: (<Notebook size={20} className='transition-all duration-300 group-hover:-translate-x-2' />)
         },
 
     ];
@@ -68,20 +73,9 @@ const Header = () => {
 
                 {/* Theme Dropdown (Mobile & Tablet) */}
                 <div className='dropdown dropdown-center md:hidden' title='Themes'>
-                    <div tabIndex={0} role='button' className='btn btn-ghost'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                        </svg>
-
-                        <svg
-                            width='12px'
-                            height='12px'
-                            className='inline-block h-2 w-2 fill-current opacity-60'
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 2048 2048'
-                        >
-                            <path d='M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z'></path>
-                        </svg>
+                    <div tabIndex={0} role='button' className='btn btn-ghost transition-all duration-300 hover:rotate-5'>
+                        <MoonStar size={20} />
+                        <ChevronDown size={20} />
                     </div>
                     <ul tabIndex={0} className='dropdown-content h-60 overflow-auto bg-base-300/90 rounded-box p-2 shadow-2xl'>
                         {themes.map((t) => (
@@ -103,16 +97,16 @@ const Header = () => {
 
                 {/* Desktop Navigation */}
                 <ul className='hidden md:flex space-x-6'>
-                    {navItems.map(({ name, slug, active }) =>
+                    {navItems.map(({ name, slug, active, icon }) =>
                         active ? (
                             <li key={slug}>
                                 <NavLink
                                     to={slug}
                                     title={name}
                                     className={({ isActive }) =>
-                                        `btn btn-block btn-ghost transition ${isActive ? 'bg-base-300 underline underline-offset-4' : 'hover:underline hover:underline-offset-4'}`}
+                                        `btn btn-block btn-ghost group flex items-center gap-2 transition ${isActive ? 'bg-base-300 underline underline-offset-4' : 'hover:underline hover:underline-offset-4'}`}
                                 >
-                                    {name}
+                                    {icon} {name}
                                 </NavLink>
                             </li>
                         ) : null
@@ -126,20 +120,10 @@ const Header = () => {
 
                 {/* Desktop Theme Dropdown (Right Side) */}
                 <div className='dropdown dropdown-center hidden md:block' title='Themes'>
-                    <div tabIndex={0} role='button' className='btn btn-ghost'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                        </svg>
+                    <div tabIndex={0} role='button' className='btn btn-ghost transition-all duration-300 hover:rotate-5'>
+                        <Sun size={20} className='transition-all duration-300 hover:rotate-180' />
 
-                        <svg
-                            width='12px'
-                            height='12px'
-                            className='inline-block h-2 w-2 fill-current opacity-60'
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 2048 2048'
-                        >
-                            <path d='M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z'></path>
-                        </svg>
+                        <ChevronDown size={20} />
                     </div>
                     <ul tabIndex={0} className='dropdown-content h-60 overflow-auto bg-base-300/90 rounded-box w-40 p-2 shadow-2xl'>
                         {themes.map((t) => (
@@ -161,7 +145,7 @@ const Header = () => {
 
                 {/* Mobile Menu Button */}
                 <button
-                    className='md:hidden focus:outline-none cursor-pointer'
+                    className='md:hidden focus:outline-none cursor-pointer transition-all duration-300 hover:rotate-30'
                     onClick={() => setMenubar(!menubar)}
                     aria-expanded={menubar}
                     aria-label='Toggle Menu'
@@ -169,26 +153,26 @@ const Header = () => {
                     {menubar ?
                         (<BrainCircuitIcon size={25} />)
                         :
-                        (<BrainIcon size={25} className='animate-spin' />)
+                        (<BrainIcon size={25} />)
                     }
                 </button>
             </nav>
 
             {/* Mobile Dropdown */}
             <ul className={`md:hidden bg-gray-900/10 backdrop-blur-3xl transition-all px-2 duration-300 ${menubar ? 'block' : 'hidden'}`}>
-                {navItems.map(({ name, slug, active }) =>
+                {navItems.map(({ name, slug, active, icon }) =>
                     active ? (
                         <li key={slug}>
                             <NavLink
                                 to={slug}
                                 title={name}
                                 className={({ isActive }) =>
-                                    `btn btn-block btn-ghost my-2 text-center transition ${isActive ? 'bg-base-300' : ''
+                                    `btn btn-block btn-ghost group flex items-center gap-2 my-2 text-center transition ${isActive ? 'bg-base-300' : ''
                                     }`
                                 }
                                 onClick={() => setMenubar(false)}
                             >
-                                {name}
+                                {name} {icon}
                             </NavLink>
                         </li>
                     ) : null
