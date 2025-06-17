92ef530691d11ffbd4a4a7bfcc9faa25d95814ce
TBS96 <9tbs6@proton.me>
2025-06-17 22:11:45 +0530

    fix: update team members list in Contact component and adjust animation effects conditionally
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/components/routes/Contact.jsx
+++ src/components/routes/Contact.jsx
@@ -4,15 +4,6 @@ import { Github, Mail } from 'lucide-react'
 
 const teamMembers = [
     {
-        name: 'Mahvish Ruhi',
-        roll: '23000121025, CSE',
-        role: 'Backend Developer',
-        github: 'https://github.com/Mahvish16',
-        mail: 'mailto:mahvish.ruhi@gmail.com',
-        color: 'bg-error/60',
-        image: 'https://avatars.githubusercontent.com/u/122742962?v=4',
-    },
-    {
         name: 'Sayan Bhattacharya',
         roll: '23000122060, CSE',
         role: 'Backend Developer',
@@ -31,22 +22,31 @@ const teamMembers = [
         image: 'https://i.ibb.co/PzVThCGG/PrantiKG.jpg',
     },
     {
-        name: 'Sanchita Kar',
-        roll: '23000121019, CSE',
-        role: 'Frontend Developer',
-        github: 'https://github.com/',
-        mail: 'mailto:karsanchita50@gmail.com',
-        color: 'bg-warning/60',
-        image: 'https://i.ibb.co/RTLwtGB/sanchita.png',
+        name: 'Mahvish Ruhi',
+        roll: '23000121025, CSE',
+        role: 'Backend Developer',
+        github: 'https://github.com/Mahvish16',
+        mail: 'mailto:mahvish.ruhi@gmail.com',
+        color: 'bg-error/60',
+        image: 'https://avatars.githubusercontent.com/u/122742962?v=4',
     },
     {
         name: 'Arghyakamal Ghosh',
         roll: '23000121026, CSE',
-        role: 'Documentation Engineer',
+        role: 'Q/A Engineer',
         github: 'https://github.com/',
         mail: 'mailto:arghyakamalghosh19@gmail.com',
         color: 'bg-info/60',
         image: 'https://i.ibb.co/S4yJ8wCK/Arghyakamal-G.jpg',
+    },
+    {
+        name: 'Sanchita Kar',
+        roll: '23000121019, CSE',
+        role: 'Documentation Engineer',
+        github: 'https://github.com/',
+        mail: 'mailto:karsanchita50@gmail.com',
+        color: 'bg-warning/60',
+        image: 'https://i.ibb.co/RTLwtGB/sanchita.png',
     },
 ];
 
@@ -65,9 +65,10 @@ const Contact = () => {
                     </p>
                     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-8'>
                         {teamMembers.map(({ github, image, name, role, roll, mail }, index) => {
-                            const aos = index === 0 ? 'fade-up' : index === 1 ? 'zoom-in' : index === 2 ? 'zoom-in-right' : index === 3 ? 'zoom-out' : 'flip-down';
+                            const aos = index === 0 ? 'fade-down' : index === 1 ? 'zoom-in' : index === 2 ? 'zoom-in-right' : index === 3 ? 'zoom-out' : 'flip-down';
+                            const aosDuration = index === 1 ? '1500' : '1100';
                             return (
-                                <div key={index} data-aos={aos} className='tooltip' data-tip={name}>
+                                <div key={index} data-aos={aos} data-aos-delay='700' data-aos-duration={aosDuration} className='tooltip' data-tip={name}>
                                     <div className={`group p-6 rounded-lg shadow-lg skeleton hover:shadow-2xl hover:translate-x-1 hover:-translate-y-1 transition-all duration-300 ease-in-out`}>
                                         <div className='flex items-center space-x-4'>
                                             <img
